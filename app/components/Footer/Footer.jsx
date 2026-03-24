c2ae3becacd0f1029efb8dcd7557f06713bd05e0
EJKim <dmdwn3979@gmail.com>
2026-03-24 19:18:23 +0900

    chore: v0.0.2

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <span className={styles.version}>v0.0.1</span>
+      <span className={styles.version}>v0.0.2</span>
       <a
         className={styles.link}
         href="https://github.com/EJKim3191/team-scheduler-ai"
