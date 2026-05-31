a8eb1161fc660caceca47ae3b31449848872d438
EJKim <dmdwn3979@gmail.com>
2026-05-31 19:15:07 +0900

    fix: rls change with team select

--- app/api/team/info/route.js
+++ app/api/team/info/route.js
@@ -12,18 +12,28 @@ async function getTeam(access_token, refresh_token, additionalInfo = false) {
   if (sessionError) {
     return { success: false, message: sessionError.message };
   }
-  const response = await supabase
-    .from("team")
-    .select("team_id, team_code, team_name, last_updated");
 
-  if (response.error) {
-    return { success: false, message: response.error.message };
+  const { data: myTeams } = await supabase
+    .from("team_members")
+    .select("team_id")
+    .eq("profile_id", sessionData.user.id);
+
+  const { data: teams, error: teamsError } = await supabase
+    .from("team")
+    .select("team_id, team_code, team_name, last_updated")
+    .in(
+      "team_id",
+      myTeams.map((team) => team.team_id),
+    );
+
+  if (teamsError) {
+    return { success: false, message: teamsError.message };
   }
 
   if (additionalInfo) {
     let teamData = [];
     // 팀 갯수가 정해져있으므로 가능하나 팀이 많을 경우 수정 필요할 수 있음
-    for (const team of response.data) {
+    for (const team of teams) {
       const { success, data } = await getTeamMembersAdditionalInfo(
         access_token,
         refresh_token,
@@ -33,10 +43,14 @@ async function getTeam(access_token, refresh_token, additionalInfo = false) {
         teamData.push({ ...team, members: data.count });
       }
     }
-    return { success: true, message: response.message, teams: teamData };
+    return {
+      success: true,
+      message: "팀 정보가 조회되었습니다.",
+      teams: teamData,
+    };
   }
 
-  return { success: true, message: response.message, teams: response.data };
+  return { success: true, message: "팀 정보가 조회되었습니다.", teams: teams };
 }
 
 async function getTeamMembersAdditionalInfo(
